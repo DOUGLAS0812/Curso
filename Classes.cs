3a63e60a991af59a6953fa50603e083e2fb11b74
DOUGLAS0812 <dodisfilho@gmail.com>
2024-04-30 14:03:42 -0300

    Adicionar arquivos de projeto.

---
+++ Classes.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+
+namespace Cadastro
+{
+
+    public static class Calculos
+    {
+        public static int SomarNumeros(int a , int b)
+        {
+            return a+b;
+        }
+    }
+    public class Produto
+    {
+        private int Id; 
+
+        public string Descricao { get; set;}
+
+        //  public readonly int Estoque; -- pode ser usado para somente leitura 
+        public  int Estoque { get; set;}
+        public Produto()
+        {
+            Estoque = 1;
+        }
+        public void ImprimirDescricao()
+        {
+            Console.WriteLine(GetId() + " - " + Descricao);
+
+        }
+
+        public void SetId(int id)
+        { 
+            this.Id = id; 
+        }
+        public int  GetId()
+        {
+            return Id; 
+        }
+
+    }
+
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Endereco { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Cep { get; set; }
+
+        public void ImprimirDados()
+        {
+
+            Console.WriteLine("Codigo: " + Id);
+            Console.WriteLine("Endereco: " + Endereco);
+            Console.WriteLine("Cidade: " + Cidade);
+            Console.WriteLine("Cep: " + Cep);
+        }
+
+       
+    }
+
+    public class Funcionario : PessoaFisica
+    {
+        public string Matricula { get; set; }
+    }
+
+    public class PessoaFisica : Pessoa
+    {
+        public string CPF { get; set; }
+
+    }
+
+    public sealed class Configuracao
+    {
+        public string  Host { get; set; }
+    }
+
+    public abstract class Animal
+    {
+        public string Nome { get; set; }
+
+        public abstract string GetInformacoes();
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome animal: " + Nome);
+            Console.WriteLine("Informacoes : " + GetInformacoes());
+        }
+
+     
+    }
+    public class Cachorro : Animal
+    {
+        public override string GetInformacoes()
+        {
+            return "Cachorro é um bom amigo";
+        }
+    }
+    public record Curso(int Id, string Descricao);
+
+    public class CursoTeste
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    /*
+    public record Curso
+    {
+        public int Id { get; set; }
+        public string  Descricao { get; set; }
+
+       /* public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            if ( obj is Curso curso)
+            {
+                return Id == curso.Id && Descricao == curso.Descricao; 
+            }
+            return base.Equals(obj);
+        }
+
+        public static bool operator == (Curso a, Curso b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator != (Curso a, Curso b)
+        {
+            return !(a == b);
+        }
+       
+    }
+    */
+
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // AulaClasses();
+        //  AulaPropriedadeSomenteleitura();
+        // AulaHeranca();
+        // AulaClasseSelada();
+        // AulaClasseAbstrata();
+        AulaRecord();
+
+    }
+
+    private static void AulaRecord()
+    {
+         var curso1 = new Cadastro.Curso(1,"curso" );
+        var curso2 = curso1 with { Descricao = "Teste Novo" };
+
+        //var curso2 = new Cadastro.Curso { Id = 1, Descricao = "curso" };
+       // var curso1 = new Cadastro.CursoTeste { Id = 1,  Descricao = "Curso" };
+        // var curso2 = curso1;
+        // curso2.Descricao = "TESTE TESTE";
+       // var curso2 = new Cadastro.CursoTeste();
+       // curso2.Id = curso1.Id;
+       // curso2.Descricao = "Nova descricao";
+
+        Console.WriteLine(curso2.Descricao);
+        Console.WriteLine(curso2.Descricao);
+
+
+
+        //Console.WriteLine(curso1 ==  curso2);
+        Console.WriteLine(curso1 == curso2);
+    }
+
+    private static void AulaClasseAbstrata()
+    {
+        var cachorro = new Cadastro.Cachorro();
+        cachorro.ImprimirDados();
+    }
+
+    private static void  AulaClasseSelada()
+    {
+        /*var configuracao = new Cadastro.Configuracao();
+        configuracao.Host = "localhost";
+        */
+
+        var configuracao = new Cadastro.Configuracao
+        {
+            Host = "localhost"
+        };
+
+        Console.WriteLine(configuracao.Host);
+
+    }
+
+    private static void AulaHeranca()
+    {
+        var pessoaFisica = new Cadastro.PessoaFisica();
+        pessoaFisica.Id = 1;
+        pessoaFisica.Endereco = "Endereco teste";
+        pessoaFisica.Cidade = "Cidade Teste";
+        pessoaFisica.Cep = "123456";
+
+        pessoaFisica.ImprimirDados();
+
+    }
+
+    private static void AulaClasses()
+    {
+        var resultado = Cadastro.Calculos.SomarNumeros(2, 3);
+        Console.WriteLine(resultado);
+
+        /* var produto = new Cadastro.Produto();
+         produto.Descricao = "Teclado";
+
+         produto.ImprimirDescricao();
+         Console.WriteLine(produto.GetId());
+        */
+    }
+
+    private static void AulaPropriedadeSomenteleitura()
+    {
+        var produto = new Cadastro.Produto();
+        produto.Descricao = "Mouse";
+        //produto.Estoque = 1;
+        Console.WriteLine(produto.Estoque);
+    }
+}
